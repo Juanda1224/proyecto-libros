0b147f5b36ea75e0567aec6317bb9beb940c1de6
Juan David <juniglofa1223@gmail.com>
2022-02-10 13:35:47 -0500

    Agregar Metodos en las clases del mundo
    -Agregamos las propiedades de los atributos de Transaccion.
    -Agregamos los metodos de vender, abastecer y ejemplares vendidos
    -Agregamos metodos de dos requisitos a la clase tienda

--- tienda.py
+++ tienda.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from logging import exception
 from tokenize import Double
 
 
@@ -9,15 +10,18 @@ class Transaccion:
     def __init__(self, tipo: int, cantidad: int):
         self._tipo = tipo
         self._cantidad =cantidad
-        self._time = datetime.now
+        self._fecha = datetime.now
     @property
     def tipo(self):
         return self._tipo
 
-    @tipo.setter
-    def tipo(self, valor):
-        if valor == 1 or valor == 2:
-            self._tipo = valor
+    @property
+    def cantidad(self):
+        return self._cantidad
+    
+    @property 
+    def fecha(self):
+        return self._fecha
 
 
 class Libro:
@@ -29,6 +33,52 @@ class Libro:
         self.cantidad_actual = cantidad_actual 
         self.transacciones = list()
 
+    @property
+    def cantidad_actual(self):
+        return self.cantidad_actual
+
+    @cantidad_actual.setter
+    def cantidad_actual(self, nueva_cantidad):
+        if nueva_cantidad < 0:
+            raise Exception("La cantidad de unidades de un libro no puede ser negativa")
+
+        self.cantidad_actual = nueva_cantidad
+    
+    def vender(self, cantidad) -> bool:
+        """"
+        VEnde una cantidad dada de ejemplares del libro. Sí no hay esa cantidad, no se realiza la venta y el metodo retorna Falase. Si la venta se realiza, el metodo retorna True
+
+        """
+        if self.cantidad_actual >= cantidad:
+            self.cantidad_actual -= cantidad
+            transaccion = Transaccion(Transaccion.VENTA, cantidad)
+            self.transacciones.append(transaccion)
+            return True
+        else:
+            return False
+
+    def abastecer(self, cantidad):
+        self.cantidad_actual += cantidad
+        transaccion = Transaccion(Transaccion.ABASTECIMINETO, cantidad)
+        self.transacciones.append(transaccion)
+
+
+    def informar_ejemplares_vendidos(self) -> int:
+        if len(self.transacciones) > 0:
+            # cantidades = [t.cantidad for t in self.transacciones if t.tipo == Transaccion.VENTA]
+            # total_ejemplares = reduce(lambda x, y: x + y, cantidades)
+            # return total_ejemplares
+            cantidad_ejemplares=0
+            for trans in self.transacciones:
+                if trans.tipo == Transaccion.VENTA:
+                    cantidad_ejemplares += trans.cantidad
+            return cantidad_ejemplares
+        else:
+            return 0
+        
+
+    def __str__(self) -> str:
+        return f"ISBN: {self.isbn}\nTItulo: {self.titulo}"
 
 
 class Tienda:
@@ -36,15 +86,19 @@ class Tienda:
         self.dinero_en_caja = 1000000
         self.catalogo = dict()
 
+    def registrar_libro_en_catalogo(self, titulo, isbn, precio_compra, precio_venta, cantidad_actual):
+        if isbn not in self.catalogo.keys():
+            libro =Libro(isbn, titulo, precio_compra, precio_venta, cantidad_actual)
+            self.catalogo[isbn] = libro 
+        else:
+            raise Exception(f"Ya existe un libro con el ISBN")
+
+    def eliminar_lirbo_del_catalogo (self, isbn):
+        if isbn in self.catalogo.keys():
+            del self.catalogo[isbn]
+            else:
+                raise Exception(f"No existe un libro con el ISBN")
 
 
-
-
-if __name__ == "__main__":
-    trans_1 = Transaccion(Transaccion.VENTA, 5)
-    trans_2 = Transaccion(Transaccion.ABASTECIMINETO, 10)
-
-    print(f"Cantidad t1: {trans_1.cantidad}")
-    print(f"Cantidad t2: {trans_2.cantidad}")
-
-
+libro = Libro("1234", "El principito", 50000, 60000, 10)
+print(libro)
